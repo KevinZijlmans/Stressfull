6e8186ad38e016a0fd5460cfb64ad5092b2120a3
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-07-30 14:24:51 +0200

    added routes to main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import routes from './routes';
 
-Vue.use(VueRouter)
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  routes
+}).$mount('#app');
