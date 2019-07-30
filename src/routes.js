862dbd20f32f62a14575440eef9f45e583fac8c8
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-07-30 14:36:00 +0200

    Added paths to routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,11 @@
-const routes = [];
+import Home from './components/Home'
+import SecondPage from './components/SecondPage'
+import ThirdPage from './components/ThirdPage'
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/secondpage', component: SecondPage },
+    { path: '/thirdpage', component: ThirdPage },
+];
 
 export default routes;
