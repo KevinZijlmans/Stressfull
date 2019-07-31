8fd01130ca8e14e8be934ab14f3d9c69b4204e10
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-07-31 21:16:30 +0200

    Added failure page

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import Home from './components/Home'
 import SecondPage from './components/SecondPage'
 import ThirdPage from './components/ThirdPage'
+import Failure from './components/Failure'
 
 const routes = [
     { path: '/', component: Home, name: 'home' },
     { path: '/secondpage', component: SecondPage, name: 'second' },
     { path: '/thirdpage', component: ThirdPage, name: 'third' },
+    { path: '/failure', component: Failure, name: 'failure'}
 ];
 
 export default routes;
