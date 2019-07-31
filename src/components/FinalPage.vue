15d62ef82caa4d7729325982c659659e8560c006
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-07-31 21:47:28 +0200

    added buttons to final page

--- src/components/FinalPage.vue
+++ src/components/FinalPage.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         Are you still stressed?
+        <br />
+        <button>HELL NO</button>
+        <br />
+        <button>Still pretty stressed yeah</button>
     </div>
 </template>
 <script>
