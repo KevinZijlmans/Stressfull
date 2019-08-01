8379cb7ab10a79e577f367842d97b191c079a381
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-08-01 11:48:29 +0200

    Added routing to thirdpage

--- src/components/ThirdPage.vue
+++ src/components/ThirdPage.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
+        <router-link to='/failure'>
         <p class='click'>Click</p>
+        </router-link>
         <i class='here'>here</i>
         <p class='to'>to</p>
         <a class='go'>go </a>
         <p class='to'>to</p>
         <p class='the'>the</p>
+        <router-link to='/failure'>
         <button class='next'>next</button>
+        </router-link>
         <p class='page'>page</p>
         <router-link to='/fourthpage'>
         <p class='therightclick'>here</p>
